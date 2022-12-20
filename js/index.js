f7f73cfa7edc0b6ce4322004ae851d61904b24e2
kevit-milan-solanki <milan.solanki.kevit@gmail.com>
2022-12-20 15:49:46 +0530

    3

--- js/index.js
+++ js/index.js
@@ -1,27 +1,37 @@
-const   API_url ="https://reqres.in/api/users?page=1&&per_page=15";
-const fetchPromise = fetch(API_url);
+const apiUrl = "https://reqres.in/api/users?page=1&&per_page=15";
+const fetchPromise = fetch(apiUrl);
 fetchPromise.then(response => {
     return response.json();
-}).then(people => {
-    let contect = (people.data);
-    let table = document.getElementById('myTable')
-    for (let i = 0; i < contect.length; i++) {
-        let row = `<tr style="">
-                            <td ><img src=${contect[i].avatar} height=50 width=50 ></img></td>
-							<td>${contect[i].first_name}</td>
-							<td>${contect[i].last_name}</td>
-							<td>${contect[i].email}</td>
-							
-					  </tr>`
-        table.innerHTML += row
-    }
+}).then(peopleContactInformation => {
+    contacts(peopleContactInformation);
 });
 
+function contacts(peopleInformation) {
+    let peopleContact = (peopleInformation.data);
+    let peopleInformationTable = document.getElementById('contactInformation')
+    for (let i = 0; i < peopleContact.length; i++) {
+        let contactInformationRow = document.createElement("tr");
 
+        let contactAvtar = document.createElement("td");
+        let avtarImage = document.createElement("img");
+        avtarImage.src = peopleContact[i].avatar;
+        contactAvtar.classList.add("contactTable_body_image")
 
+        let contactFirstname = document.createElement("td");
+        let firstName = document.createTextNode(peopleContact[i].first_name);
+        let contactLastName = document.createElement("td");
+        let lastName = document.createTextNode(peopleContact[i].last_name);
 
+        contactInformationRow.appendChild(contactAvtar);
+        contactAvtar.appendChild(avtarImage);
 
+        contactInformationRow.appendChild(contactFirstname);
+        contactFirstname.appendChild(firstName);
 
+        contactInformationRow.appendChild(contactLastName);
+        contactLastName.appendChild(lastName);
 
+        peopleInformationTable.appendChild(contactInformationRow);
 
-
+    }
+}
